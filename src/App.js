ccdce185314365a815cb01a4a0d1693e2cd2c6d1
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-06-13 21:57:10 +0200

    Added dog breed list with images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer'
+import { Route } from 'react-router-dom'
+// import DogBreedImages from './components/DogBreedImages';
+import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <main>
-          <DogsListContainer />
+          <Route exact path="/" component={DogsListContainer} />
+          <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
         </main>
-        
       </div>
     );
   }
---
+++ src/components/DogBreedImages.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+export default class  DogBreedImages extends Component {
+  
+    renderDogBreedImages (images) {
+      return <li key={images}>
+                 <Link to={ `/dog-images/${images}` }>{images}</Link>
+            </li>
+    }
+      render () {
+        const {images} = this.props
+       return (
+    <div className="dog-breed-images">
+      <h1>Dogs Breed Images</h1>
+      
+      { !images && 'Loading...' } 
+      
+      { 
+        images && images
+        .map(url => <img src={ url } alt="Dog" />) }
+     
+      
+    </div>
+  )
+}
+}
